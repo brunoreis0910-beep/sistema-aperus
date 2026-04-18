bf50aa72a6d7cec9c05b306d4c22438c9e13debf
Bruno Reis <brunoreis0910@beep.com.br>
2026-04-18 11:22:27 -0300

    Corrigir politica de execucao PowerShell no ATUALIZAR.bat

--- ATUALIZAR.bat
+++ ATUALIZAR.bat
@@ -3,7 +3,7 @@ title APERUS - ATUALIZAR SERVIDOR
 if not exist "%~dp0logs" mkdir "%~dp0logs"
 set LOGFILE=%~dp0logs\atualizar_saida.txt
 echo Iniciando atualizacao em %date% %time% > "%LOGFILE%"
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0ATUALIZAR.ps1" >> "%LOGFILE%" 2>&1
+powershell.exe -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force; & '%~dp0ATUALIZAR.ps1'" >> "%LOGFILE%" 2>&1
 echo. >> "%LOGFILE%"
 echo === FIM === >> "%LOGFILE%"
 notepad "%LOGFILE%"
